727efffff8f12c8c7900df7e4b26c78d506f4f4e
devdmitchell <dmitchell.prsvr@gmail.com>
2025-01-08 10:27:09 -0600

    in class commit, initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const logger = require('morgan') 
+const indexRouter = require('./routes/indexRouter')
+
+const port = 3000
+
+const app = express()              
+app.use(logger('dev'))   
+app.use(express.json()) 
+app.use('/', indexRouter) 
+app.use('/api/game', gameRouter)
+
+
+app.listen(port, ()=>{                        
+    console.log(`Server Started on port ${port}.`)
+})
---
+++ routes/gameRouter.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const uuidv4 = require('uuid').v4
+
+
+let games = [
+    {
+        id: "adowb1b3bb",
+        game: "League of Legends",
+        description: "League of Legends is a team-based game with over 140 champions to make epic plays with."
+    },
+    {
+        id: "kd7b9ks2nda",
+        game: "PlayerUnknown's Battlegrounds",
+        description: "PLAYERUNKNOWN'S BATTLEGROUNDS is a last-man-standing shooter being developed with community feedback."
+    }
+    ]
+
+    router.get(`/get-all-games`, (req, res)=>{
+        res.json({payload: games})
+    })
+
+    router.get('/get-game-by-id/:id', (req, res)=>{
+        const { id } = req.params
+        const game = games.find(game => game.id === id)
+        if (game) {
+            res.json({message: 'Game found.', payload: game})
+        }else{
+            res.json({message: "The game with the id does not exist, please check id"})
+        }
+    })
+
+    router.get(`/get-game-by-id/:id`, (req, res)=>{
+        const { id } = req.params
+        const gameItem = games.find((game)=> game.id === id)
+        if (!gameItem) {
+            return res.json({message:'The game with the id does not exist, please check id'})
+        }
+        res.json({message: "Game found", payload: gameItem })
+      })
+    
+      router.post('/create-new-game', (req, res) => {
+        const game = req.body.description
+        const description = req.body.description
+        
+        const newGame = {
+          id: uuidv4(),
+          game: req.body.game,
+          description: req.body.description
+        }
+       games.push(newGame)
+        res.json({message:"Game added.", payload: games})
+      })
+    
+
+    
+module.exports = router
