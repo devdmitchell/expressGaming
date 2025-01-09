cdd1bec2d1266a015d6d2e9286a219b5a54a3c69
devdmitchell <dmitchell.prsvr@gmail.com>
2025-01-09 09:51:57 -0600

    in class push

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -37,9 +37,9 @@ let games = [
             return res.json({message:'The game with the id does not exist, please check id'})
         }
         res.json({message: "Game found", payload: gameItem })
-      })
+    })
     
-      router.post('/create-new-game', (req, res) => {
+    router.post('/create-new-game', (req, res) => {
         const game = req.body.description
         const description = req.body.description
         if(game === undefined || description === undefined){
@@ -47,20 +47,46 @@ let games = [
         }else if(games.find(element => element.game === game)){
             res.json({message:"Game already exists, cannot add game"})
         }else{
-        const newGame = {
-            id: uuidv4(),
-            game: req.body.game,
-            description: req.body.description
+            const newGame = {
+                id: uuidv4(),
+                game: req.body.game,
+                description: req.body.description
+            }
+            games.push(newGame)
+            res.json({message:"Game added.", payload: games})
         }
-        games.push(newGame)
-        res.json({message:"Game added.", payload: games})
-         }})
+    })
     
     
     // const {game, description} = req.body   //destructuring
     // // if(game || description)
     // if(game === undefined || description === undefined || games.find(element => element.game === game))
     //     res.json({game, description})
+
+    router.put('/update-game/:id', function (req, res) {
+        const id = req.params.id
+        const { game, description } = req.body
+        const gameUpdate = games.find(gameElement => gameElement.id === id)
+        if (!gameUpdate) {
+            return res.json({message: "Game not found, cannot update"})
+        }else if (game !== undefined && game !== ""){
+            gameUpdate.game = game
+        }else (description !== undefined && description !== ""){
+            gameUpdate.description = description
+            res.json({message: "Game updated successfully", payload: gameUpdate})
+        }
+    })
+
+    router.delete('/delete-game/:id', function (req, res) {
+        const id = req.params.id
+        const gameToDelete = games.find(game => game.id === id)
+        if (!gameToDelete) {
+            res.json({message: "Game not found, cannot delete"})
+        }else{
+            games = games.filter(game => game.id !== id)
+            res.json({message: "Game deleted successfully"})
+        }
+    })
 
     
 module.exports = router
